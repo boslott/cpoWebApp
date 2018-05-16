67290565405a7bfd0acb2b1326d130e1cbb3bd02
= <=>
2018-05-16 19:57:25 -0400

    create registration form, add registration form to login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row } from 'react-bootstrap';
 
 import Navbar from '../../Components/Navbar/Navbar';
 import LoginForm from '../../Components/LoginForm/LoginForm';
+import RegistrationForm from '../../Components/RegistrationForm/RegistrationForm';
 
 export default class Login extends Component {
   render() {
@@ -10,13 +11,11 @@ export default class Login extends Component {
       <section className='login'>
         <Navbar />
         <Grid>
-          <Row>
-            <Col xs={12} sm={6} className='loginCol'>
-              <LoginForm history={this.props.history}/>
-            </Col>
-            <Col xs={12} sm={6}>
-              <p>Registration Form!</p>
-            </Col>
+          <Row className='form-row'>
+            <LoginForm history={this.props.history} />
+          </Row>
+          <Row className='form-row'>
+            <RegistrationForm history={this.props.history} />
           </Row>
         </Grid>
       </section>
