6a684256194a38b33ea13ca5b9fc12a0dc00e6d5
= <=>
2018-05-17 10:42:52 -0400

    create registration page with registration form, connect already-account-link, redirect to dashboard, create dashboard page, create db-side and db-content components

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -28,28 +28,35 @@ export default class LoginForm extends Component {
     return (
       <form className='form'>
         <h2 className='form__title'>Login</h2>
-        <FormGroup controlId='login-email'>
-          <FormControl
-            type='email'
-            name='email'
-            bsSize='large'
-            value={this.state.email}
-            onChange={this.handleInputChange}
-            placeholder='Email'
-          />
-        </FormGroup>
-        <FormGroup controlId='login-password'>
-          <FormControl
-            type='password'
-            name='password'
-            bsSize='large'
-            value={this.state.password}
-            onChange={this.handleInputChange}
-            placeholder='Password'
-          />
-        </FormGroup>
-        <Row className='btn-row'>
+        <Row>
+          <FormGroup controlId='login-email'>
+            <FormControl
+              type='email'
+              name='email'
+              bsSize='large'
+              value={this.state.email}
+              onChange={this.handleInputChange}
+              placeholder='Email'
+            />
+          </FormGroup>
+          <FormGroup controlId='login-password'>
+            <FormControl
+              type='password'
+              name='password'
+              bsSize='large'
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              placeholder='Password'
+            />
+          </FormGroup>
+        </Row>
+        <Row className='text-center formbtn__row'>
           <a className='formbtn' onClick={this.handleFormSubmit}>Submit</a>
+        </Row>
+        <Row className='text-center'>
+          <p className='form__link-para'>
+            No account? <a className='form__link' onClick={() => this.props.history.push('/registration')}>START</a> now
+          </p>
         </Row>
       </form>
     );
