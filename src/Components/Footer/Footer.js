31e629d9618d649828b8e784be20b7049a9d519f
= <=>
2018-05-16 20:17:25 -0400

    create footer and add to login page

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../../Sass/index.css';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer className='footer'>
+        <h5 className='copyright'>&copy; Copyright 2018 CPOLog.com - All Rights Reserved</h5>
+      </footer>
+    );
+  }
+}
