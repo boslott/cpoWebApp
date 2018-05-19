0a78b604424455a80e65589a0a628354a7d95f55
= <=>
2018-05-19 11:51:19 -0400

    create footer with help button

--- src/Components/DBSidePanel/DBSPNavLink/DBSPNavLink.js
+++ src/Components/DBSidePanel/DBSPNavLink/DBSPNavLink.js
@@ -4,44 +4,19 @@ import { Row, Col } from 'react-bootstrap';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 import '../../../Sass/index.css';
-import DBSPNavDropdown from '../DBSPNavDropdown/DBSPNavDropdown';
 
 export default class DBSPNavLink extends Component {
-
-  state = {
-    dropdown: this.props.dropdown || false,
-  }
-
   render() {
     return (
       <Row>
-        {this.state.dropdown ? (
-          <div>
-            <div className='dbsp-navlink'>
-              <Col xs={2} className='dbsp-navlink__icon'>
-                <FontAwesomeIcon icon={this.props.icon} size='lg' />
-              </Col>
-              <Col xs={8}>
-                {this.props.linkText}
-              </Col>
-              <Col xs={2}>
-                <FontAwesomeIcon icon='chevron-down' size='sm' />
-              </Col>
-            </div>
-            <Row>
-              <DBSPNavDropdown list={this.props.list} />
-            </Row>
-          </div>
-        ) : (
-          <Link className='dbsp-navlink' to={this.props.linkPath}>
-            <Col xs={2} className='dbsp-navlink__icon'>
-              <FontAwesomeIcon icon={this.props.icon} size='lg' />
-            </Col>
-            <Col xs={8}>
-              {this.props.linkText}
-            </Col>
-          </Link>
-        )}
+        <Link className='dbsp-navlink' to={this.props.linkPath}>
+          <Col xs={2} className='dbsp-navlink__icon'>
+            <FontAwesomeIcon icon={this.props.icon} size='lg' />
+          </Col>
+          <Col xs={8}>
+            {this.props.linkText}
+          </Col>
+        </Link>
       </Row>
     );
   }
